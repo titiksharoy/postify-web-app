f59bbb4d6e1f55b0a284b4ff28a4c89bcf23a40a
Titiksha Roy <titiksharoy2004@gmail.com>
2026-07-25 03:32:30 +0530

    Simplify MongoDB connection setup

--- app.js
+++ app.js
@@ -96,19 +96,9 @@ app.get('/profile', isLoggedIn, async function(req,res){
     res.render("profile",{user});
 });
 
-/*app.get("/feed", isLoggedIn, async function(req, res){
-let user = await userModel.findOne({ email: req.user.email });
-    let posts = await postModel.find().populate("user");
-    res.render("feed", { posts, user });
-
-});*/
-app.get("/feed", isLoggedIn, async function(req, res){
+app.get("/feed", isLoggedIn, async function(req, res){ 
     let user = await userModel.findOne({ email: req.user.email });
-
     let posts = await postModel.find().populate("user");
-
-   
-    posts = posts.filter(post => post.user);
 
     res.render("feed", { posts, user });
 });
--- config/mongoose-configuration.js
+++ config/mongoose-configuration.js
@@ -1,27 +1,5 @@
 const mongoose = require("mongoose");
 
-console.log("Connecting to MongoDB...");
-console.log("URI exists:", !!process.env.MONGODB_URI);
-
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on("connected", () => {
-  console.log("✅ MongoDB connected");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error("❌ MongoDB Error:", err);
-});
-
-mongoose.connection.on("disconnected", () => {
-  console.log("⚠️ MongoDB disconnected");
-});
-
-/*const mongoose = require("mongoose");
-const dns = require("dns");
-
-dns.setServers(["8.8.8.8"]);
-
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("MongoDB connected"))
-.catch((err) => console.log(err));*/
+.catch((err) => console.log(err));
